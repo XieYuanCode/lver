11fd5c833bd71d6ad2c292784cd9e4faad35da29
zerocsss <1756297001@qq.com>
2021-10-25 17:30:52 +0800

    refactor  main.ts

--- source/lver/src/render/main.ts
+++ source/lver/src/render/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import naive from "naive-ui"
-import contextmenu from 'vue3-contextmenu'
-import 'vue3-contextmenu/dist/vue3-contextmenu.css'
+import { create, NButton } from "naive-ui"
+// import contextmenu from 'vue3-contextmenu'
+// import 'vue3-contextmenu/dist/vue3-contextmenu.css'
 import i18n from "./locale";
 
 import 'vfonts/FiraCode.css'
@@ -11,11 +11,15 @@ import "./style/init.css"
 
 import { store, key } from "./store"
 
+const naive = create({
+  components: [NButton]
+})
+
 const app = createApp(App)
 
 app.use(naive)
 app.use(store, key)
-app.use(contextmenu)
+// app.use(contextmenu)
 app.use(i18n)
 
 app.mount("#app")
