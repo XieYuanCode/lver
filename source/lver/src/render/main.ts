66f4f5816f1684f689c793b410d27d0ddaea81f4
谢渊 <xieyuan@agree.com.cn>
2021-10-25 17:26:26 +0800

    修复部分类型错误 删除vue3-contextmeng的引用

--- source/lver/src/render/main.ts
+++ source/lver/src/render/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import naive from "naive-ui"
-import contextmenu from 'vue3-contextmenu'
-import 'vue3-contextmenu/dist/vue3-contextmenu.css'
 import i18n from "./locale";
 
 import 'vfonts/FiraCode.css'
@@ -15,7 +13,6 @@ const app = createApp(App)
 
 app.use(naive)
 app.use(store, key)
-app.use(contextmenu)
 app.use(i18n)
 
 app.mount("#app")
